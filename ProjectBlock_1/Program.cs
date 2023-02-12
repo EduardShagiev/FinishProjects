022fcef3c110d7b41cdd4ae86da44ca11b93616c
EduardSh <freedom886@mail.ru>
2023-02-12 18:42:31 +0500

    add folder and ptoject

---
+++ ProjectBlock_1/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine() ?? "";
+M = stroka.Split();
+string[] result = new string[M .Length];
+int realSize = 0;   
+ foreach (var value in M )
+        {
+            if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+        }
+ for (int i = 0; i < result.Length;i++)
+ {
+     Console.Write(result[i]+ " ");
+ }
